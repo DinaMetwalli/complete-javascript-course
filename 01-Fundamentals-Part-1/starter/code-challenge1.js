697bebed16be45c04d685792f77b86e25a1abd93
Metwalli <dina.moh657@gmail.com>
2024-09-24 11:53:16 +0100

    add code challenge 1

--- 01-Fundamentals-Part-1/starter/code-challenge1.js
+++ 01-Fundamentals-Part-1/starter/code-challenge1.js
@@ -1,19 +1,11 @@
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
-const heightJohn = 1.76;
+const heightJohn = 1.95;
 
-let BMIMark = massMark / (heightMark * heightMark);
-let BMIJohn = massJohn / (heightJohn * heightJohn);
-
-let markHigherBMI = false;
-
-if (BMIMark > BMIJohn) {
-    markHigherBMI = true;
-}
-else {
-    markHigherBMI = false;
-}
+const BMIMark = massMark / (heightMark ** 2);
+const BMIJohn = massJohn / (heightJohn ** 2);
+const markHigherBMI = BMIMark > BMIJohn;
 
 console.log("Mark's BMI = ", BMIMark);
 console.log("John's BMI = ", BMIJohn);
